5302cc9accdf3a0ec03dcd7b7573c8d5451b5c38
Maheshkrishna A G <maheshkrishnagopal@gmail.com>
2019-05-18 14:03:51 +0530

    Solution for Utopian Tree

---
+++ Python-Algorithms-Implementation-Utopian Tree.py
@@ -0,0 +1,29 @@
+# https://www.hackerrank.com/challenges/utopian-tree
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the utopianTree function below.
+def utopianTree(n):
+    h = 1
+    for i in range(1,n+1):
+        if i%2 != 0:
+            h *= 2
+        else:
+            h += 1
+    return h
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    t = int(input())
+    for t_itr in range(t):
+        n = int(input())
+        result = utopianTree(n)
+        fptr.write(str(result) + '\n')
+    fptr.close()
