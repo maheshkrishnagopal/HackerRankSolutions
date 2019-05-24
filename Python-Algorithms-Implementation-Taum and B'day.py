7412214fae3ff4a33e3e818976b7dd93c42b532f
Maheshkrishna A G <maheshkrishnagopal@gmail.com>
2019-05-24 10:08:21 +0530

    Solution for Taum and B'day

---
+++ Python-Algorithms-Implementation-Taum and B'day.py
@@ -0,0 +1,27 @@
+# https://www.hackerrank.com/challenges/taum-and-bday
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the taumBday function below.
+def taumBday(b, w, bc, wc, z):
+   return min(b*bc, b*(wc+z)) + min(w*(bc+z), w*wc)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    t = int(input())
+    for t_itr in range(t):
+        bw = input().split()
+        b = int(bw[0])
+        w = int(bw[1])
+        bcWcz = input().split()
+        bc = int(bcWcz[0])
+        wc = int(bcWcz[1])
+        z = int(bcWcz[2])
+        result = taumBday(b, w, bc, wc, z)
+        fptr.write(str(result) + '\n')
+    fptr.close()
