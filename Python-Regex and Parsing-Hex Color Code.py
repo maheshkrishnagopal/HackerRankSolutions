bd6f0ac694a0e04852d396f49c6ab51dde1e0d68
Maheshkrishna A G <maheshkrishnagopal@gmail.com>
2019-05-27 17:17:54 +0530

    Solution for Hex Color Code

---
+++ Python-Regex and Parsing-Hex Color Code.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/hex-color-code
+
+import re
+for i in range(int(input())):
+    matches = re.findall(r':?.(#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3})', input());
+    if matches:
+        print(*matches, sep='\n');
