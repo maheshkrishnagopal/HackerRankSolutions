924094e5240b8f7743d2335f58f0b7f184427f7a
Maheshkrishna A G <maheshkrishnagopal@gmail.com>
2019-05-27 17:13:09 +0530

    Solution for Gemstones

---
+++ Python-Algorithms-Strings-Gemstones.py
@@ -0,0 +1,27 @@
+# https://www.hackerrank.com/challenges/gem-stones
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the gemstones function below.
+def gemstones(arr):
+    result = set(arr[0])
+
+    for curr in arr[1:]:
+        result.intersection_update(curr)
+    return len(result)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    n = int(input())
+    arr = []
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+    result = gemstones(arr)
+    fptr.write(str(result) + '\n')
+    fptr.close()
