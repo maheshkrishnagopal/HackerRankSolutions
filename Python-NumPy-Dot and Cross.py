5e6d84ed6255ef36ac6014b182919ed3a32e7a48
Maheshkrishna A G <maheshkrishnagopal@gmail.com>
2019-05-30 13:39:47 +0530

    Solution for Dot and Cross

---
+++ Python-NumPy-Dot and Cross.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/np-dot-and-cross
+
+import numpy
+
+N = int(input())
+A = numpy.array([list(map(int,input().split())) for _ in range(N)])
+B = numpy.array([list(map(int,input().split())) for _ in range(N)])
+print(numpy.dot(A, B))
