628c603a9b66e7982c473f0fb42fb1c2edce9184
Maheshkrishna A G <maheshkrishnagopal@gmail.com>
2019-05-30 13:09:37 +0530

    Solution for Floor, Ceil and Rint

---
+++ Python-Numpy-Floor, Ceil and Rint.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/floor-ceil-and-rint
+
+import numpy
+
+numpy.set_printoptions(sign=' ')
+
+arr = numpy.array(input().split(), float)
+print(numpy.floor(arr), numpy.ceil(arr), numpy.rint(arr), sep='\n')
